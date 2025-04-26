c921501729d6d866e525700df218522c240c11a2
caokhanhquynh <kqc@sfu.ca>
2025-04-26 15:36:42 +0700

    add water drop

--- game.py
+++ game.py
@@ -34,9 +34,16 @@ plant_frames = [
 ]
 plant_frame_index = 0
 
+# water drop frame
+water_frame_path = "./assets/water_drop.png"
+water_frame = pygame.transform.scale(pygame.image.load(water_frame_path).convert_alpha(), (50, 50))
+
 # Set up the clock (for FPS)
 clock = pygame.time.Clock()
 fps = 95
+
+water_visible = False
+click_time = None
 
 # Main game loop
 running = True
@@ -47,6 +54,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # Check for mouse click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            click_time = pygame.time.get_ticks()
 
     # Update animation frame
     if frame_timer >= frame_delay:
@@ -58,6 +69,16 @@ while running:
     # Draw current frame as background
     screen.blit(frames[frame_index], (0, 0))
     screen.blit(plant_frames[plant_frame_index], (540, 320))
+    
+    if click_time is not None:
+        current_time = pygame.time.get_ticks()
+        if current_time - click_time <= 1000:
+            water_visible = True
+            mouse_y += 0.3
+        else:
+            water_visible = False
+    if water_visible:
+        screen.blit(water_frame, (mouse_x, mouse_y))
 
     # Update display
     pygame.display.flip()

8f3042ca1a83cb6ffb16e86f8d98a25309a8e70e
caokhanhquynh <kqc@sfu.ca>
2025-04-26 14:13:22 +0700

    add plant

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ screen_width, screen_height = 1200, 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Watering Quinnie's Tree")
 
+# background frames
 frame_folder = "./assets/bg"
 frame_files = sorted([f for f in os.listdir(frame_folder) if f.startswith("frame_") and f.endswith(".png")])
 frames = [
@@ -17,10 +18,21 @@ frames = [
     )
     for f in frame_files
 ]
-
 frame_index = 0
 frame_timer = 0
 frame_delay = 100
+
+# plant frames
+plant_frame_path = "./assets/plant"
+plant_frame_files = sorted([f for f in os.listdir(plant_frame_path) if f.startswith("frame_") and f.endswith(".png")])
+plant_frames = [
+    pygame.transform.scale(
+        pygame.image.load(os.path.join(plant_frame_path, f)).convert_alpha(),
+        (200, 200)
+    )
+    for f in plant_frame_files
+]
+plant_frame_index = 0
 
 # Set up the clock (for FPS)
 clock = pygame.time.Clock()
@@ -36,13 +48,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # # Update animation frame
+    # Update animation frame
     if frame_timer >= frame_delay:
         frame_index = (frame_index + 1) % len(frames)
         frame_timer = 0
+        plant_frame_index = (plant_frame_index + 1) % len(plant_frames)
+        plant_frame_timer = 0
 
     # Draw current frame as background
     screen.blit(frames[frame_index], (0, 0))
+    screen.blit(plant_frames[plant_frame_index], (540, 320))
 
     # Update display
     pygame.display.flip()

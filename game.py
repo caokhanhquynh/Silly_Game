db1bdb95d055008c613335f6a9888a4cf203f2df
caokhanhquynh <kqc@sfu.ca>
2025-06-04 23:40:19 -0700

    change background

--- game.py
+++ game.py
@@ -4,20 +4,13 @@ import os
 
 pygame.init()
 
-screen_width, screen_height = 1200, 700
+screen_width, screen_height = 700, 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Watering Quinnie's Tree")
 
 # background frames
-frame_folder = "./assets/bg"
-frame_files = sorted([f for f in os.listdir(frame_folder) if f.startswith("frame_") and f.endswith(".png")])
-frames = [
-    pygame.transform.scale(
-        pygame.image.load(os.path.join(frame_folder, f)).convert(),
-        (screen_width, screen_height)
-    )
-    for f in frame_files
-]
+frame_folder = "./assets/cafebg.png"
+frame = pygame.transform.scale(pygame.image.load(frame_folder).convert_alpha(), (700, 700))
 frame_index = 0
 frame_timer = 0
 frame_delay = 100
@@ -28,12 +21,12 @@ plant_frame_files = sorted([f for f in os.listdir(plant_frame_path) if f.startsw
 plant_frames = [
     pygame.transform.scale(
         pygame.image.load(os.path.join(plant_frame_path, f)).convert_alpha(),
-        (200, 200)
+        (230, 230)
     )
     for f in plant_frame_files
 ]
 plant_frame_index = 0
-plant_x, plant_y = 540, 320
+plant_x, plant_y = 78, 335
 plant_width, plant_height = plant_frames[0].get_size()
 plant_center_x = plant_x + plant_width // 2
 plant_center_y = plant_y + plant_height // 2
@@ -46,10 +39,10 @@ cat_frame_timer = 0
 cat_frame_delay = 300
 cat_frame_path = "./assets/cat_opened_eyes.png"
 cat_frame_path_2 = "./assets/cat_opened_eyes2.png"
-cat_frames = [pygame.transform.scale(pygame.image.load(cat_frame_path).convert_alpha(), (150, 150)), pygame.transform.scale(pygame.image.load(cat_frame_path_2).convert_alpha(), (150, 150))]
+cat_frames = [pygame.transform.scale(pygame.image.load(cat_frame_path).convert_alpha(), (180, 180)), pygame.transform.scale(pygame.image.load(cat_frame_path_2).convert_alpha(), (180, 180))]
 cat_closed_eye = "./assets/cat_closed_eyes.png"
-cat_closed_eye_frame = pygame.transform.scale(pygame.image.load(cat_closed_eye).convert_alpha(), (150, 150))
-cat_x, cat_y = 800, 400
+cat_closed_eye_frame = pygame.transform.scale(pygame.image.load(cat_closed_eye).convert_alpha(), (180, 180))
+cat_x, cat_y = 520, 360
 cat_width, cat_height = cat_frames[0].get_size()
 cat_center_x = cat_x + cat_width // 2
 cat_center_y = cat_y + cat_height // 2
@@ -85,7 +78,6 @@ while running:
 
     # Animation frame
     if frame_timer >= frame_delay:
-        frame_index = (frame_index + 1) % len(frames)
         frame_timer = 0
         plant_frame_index = (plant_frame_index + 1) % len(plant_frames)
         plant_frame_timer = 0
@@ -96,7 +88,7 @@ while running:
         cat_frame_timer = 0
 
     # Draw current frame as background
-    screen.blit(frames[frame_index], (0, 0))
+    screen.blit(frame, (0, 0))
     screen.blit(plant_frames[plant_frame_index], (plant_x, plant_y))
     
     # Mouse click condition
@@ -105,6 +97,7 @@ while running:
         
         dy = mouse_y - cat_center_y
         dx = mouse_x - cat_center_x
+        
         # Cat closed eyes condition
         if abs(dx) <= cat_horizontal_bound and abs(dy) <= cat_vertical_bound and current_time - click_time <= 500:
             cat_closed_eye_visible = True
@@ -114,7 +107,8 @@ while running:
         # Water drop condition
         dy = mouse_y - plant_center_y
         dx = mouse_x - plant_center_x
-        if abs(dx) <= plant_horizontal_bound and abs(dy) <= plant_vertical_bound and current_time - click_time <= 1000:
+        in_plant_range = abs(dx) <= plant_horizontal_bound and abs(dy) <= plant_vertical_bound
+        if in_plant_range and current_time - click_time <= 1000:
             water_visible = True
             mouse_y += 0.3 # move the drop slowly down
         else:
